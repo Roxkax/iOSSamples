869a1a4e912a3930833708514f3fa25c25feb2d8
Diogo Gonçalves <dgoncalves@agap2.pt>
2022-06-03 21:31:46 +0100

    Added base iOS project

---
+++ iOSSamples/iOSSamples/iOSSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSSamplesApp.swift
+//  iOSSamples
+//
+//  Created by Diogo Gonçalves on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
