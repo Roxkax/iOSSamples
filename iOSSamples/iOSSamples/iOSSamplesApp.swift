f8e8ab6ab4ac52d208f287bbffd25b3cfb4a4830
Diogo Gonçalves <dgoncalves@agap2.pt>
2022-06-04 18:20:25 +0100

    @iOSSamplesApp: Added logic to add and subtract a counter

--- iOSSamples/iOSSamples/iOSSamplesApp.swift
+++ iOSSamples/iOSSamples/iOSSamplesApp.swift
@@ -7,11 +7,76 @@
 
 import SwiftUI
 
+class AppState : ObservableObject {
+    @Published var count = 0
+}
+
 @main
 struct iOSSamplesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(state: AppState())
         }
+    }
+}
+
+private func ordinal(_ n:Int) -> String {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .ordinal
+    return formatter.string(for: n) ?? ""
+}
+
+struct ContentView : View {
+    @ObservedObject var state : AppState
+    
+    var body: some View {
+        NavigationView {
+            List{
+                NavigationLink(destination: CounterView(state: self.state)) {
+                    Text("Counter demo")
+                }
+                NavigationLink(destination: FavouritePrimesView()) {
+                    Text("Favorite primes")
+                }
+            }.navigationBarTitle("State managment")
+        }
+    }
+}
+
+struct CounterView : View {
+    @ObservedObject var state : AppState
+    
+    var body: some View{
+        VStack {
+            HStack {
+                Button(action: { self.state.count -= 1 }) {
+                    Text("-")
+                }
+                Text("\(self.state.count)")
+                Button(action: { self.state.count += 1 }) {
+                    Text("+")
+                }
+            }
+            Button(action: {}) {
+                Text("Is this prime?")
+            }
+            Button(action: {}) {
+                Text("What is the \(ordinal(self.state.count)) prime?")
+            }
+        }
+        .font(.title)
+        .navigationBarTitle("Counter Demo")
+    }
+}
+
+struct FavouritePrimesView : View{
+    var body: some View{
+        EmptyView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(state: AppState())
     }
 }
